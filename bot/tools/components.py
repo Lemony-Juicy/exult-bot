684c86cb91c4f0e1acfa80b4cfd81008e5bd03b4
Andrew <andrewg.mason2709@gmail.com>
2022-02-01 21:39:35 +0000

    Added entire warning feature

--- bot/tools/components.py
+++ bot/tools/components.py
@@ -503,3 +503,43 @@ class SuggestVotes(discord.ui.View):
         embed.set_field_at(2, name="Total", value=total, inline=True)
         msg = await interaction.channel.fetch_message(interaction.message.id)
         await msg.edit(embed=embed, view=SuggestVotes(self.bot))
+
+class Paginator(discord.ui.View):
+    def __init__(self, ctx, pages, page=0, start_end=False, step_10=False):
+        super().__init__(timeout=120)
+        self.ctx = ctx
+        self.page = page
+        self.pages = pages
+        self.count = len(pages)
+        self.start_end = start_end
+        self.step_10 = step_10
+        self.add_buttons()
+
+    def add_buttons(self):
+        non_page_buttons = [item for item in self.children if not isinstance(item, PaginatorButton)]
+        if self.children:
+            self.clear_items()
+        if not self.count or self.count == 1:
+            return
+        previous_page = self.page - 1
+        if previous_page < 0:
+            previous_page = self.count - 1
+        self.add_item(PaginatorButton(label="◀", page=previous_page, style=discord.ButtonStyle.red))
+        self.add_item(PaginatorButton(label=f"{self.page + 1} / {len(self.pages)}", style=discord.ButtonStyle.grey, disabled=True))
+        next_page = self.page + 1
+        if next_page > self.count - 1:
+            next_page = 0
+        self.add_item(PaginatorButton(label="▶", page=next_page, style=discord.ButtonStyle.green))
+        for item in non_page_buttons:
+            self.add_item(item)
+
+class PaginatorButton(discord.ui.Button["Paginator"]):
+    def __init__(self, label, style, row=0, page=None, disabled=False):
+        super().__init__(style=style, label=label, row=row, disabled=disabled)
+        self.page = page
+
+    async def callback(self, interaction: discord.Interaction):
+        self.pages = self.view.pages
+        self.view.page = self.page
+        self.view.add_buttons()
+        await interaction.message.edit(embed=self.pages[self.page], view=self.view)
