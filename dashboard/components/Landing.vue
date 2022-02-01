34ed6fae5d21fae6e2884c74ec90fe6501392fda
Ciaran Day <cavemangamer3@gmail.com>
2022-02-01 22:12:51 +0000

    Landing Page Full [NOT FINAL]

--- dashboard/components/Landing.vue
+++ dashboard/components/Landing.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="bg-backing bg-main h-landingfull">
+<div class="bg-backing bg-main h-full">
     <div class="bg-main w-full h-landing bg-backing">
         <div class="relative h-l-half ml-60 flex justify-between">
             <div>
@@ -17,6 +17,60 @@
     <div class="w-full h-20 mb-12 bg-blue-accent flex justify-between text-white opacity-85">
         <h1 class="text-xl pt-6 font-semibold pl-20">Currently in <a class="red-accent">84</a> servers, serving <a class="red-accent">10,204</a> people</h1>
         <button class=" bg-primary-blue h-10 px-9 rounded-lg mt-4 mr-52">Learn More</button>
+    </div>
+    <div class="flex justify-between pt-4">
+        <div class="text-white ml-72 mt-20">
+            <h1 class="text-3xl font-bold">It's time to revamp <br> your Discord server</h1>
+            <p class="mt-3 text-gray-400">The Exult Discord Bot does it all for you. <br> Sit back, relax and enjoy!</p>
+        </div>
+        <div>
+            <img class=" mr-60" src="~/static/imgs/DiscordBG.svg" alt="Discord Mockup Background">
+        </div>
+    </div>
+    <div class="flex justify-between pt-4">
+        <div class=" ml-56">
+            <img class="-mt-20" src="~/static/imgs/DashboardGFX.svg" alt="Dashboard Coming Soon Background">
+        </div>
+        <div class="text-white mr-80 mt-32">
+            <h1 class="text-3xl font-bold">You're in control...</h1>
+            <p class="mt-3 text-gray-400">A fully-customisable bot, <br> with a user-friendly dashboard!</p>
+        </div>
+    </div>
+    <div class="mt-10">
+        <h1 class="flex justify-center text-white text-4xl font-bold">Help & Resources</h1>
+        <div class="flex justify-center mt-20">
+            <div class="text-white h-96 w-80 rounded-2xl flex justify-center border border-gold-accent bg-blue-accent">
+                <div class="mt-7">
+                    <p class="text-center text-2xl font-bold">List of features</p>
+                    <p class="mt-2 p-3 text-center">The Exult Discord Bot has a wide variety of many different features.
+                        Make sure to read through the whole list to get the maximum potential out of the bot!
+                    </p>
+                    <img class="ml-3" src="~/static/imgs/BPs.svg" alt="List Preview Mockup">
+                    <button class="bg-primary-blue p-3 px-5 rounded-xl ml-24 mt-3">See full list</button>
+                </div>
+            </div>
+            <div class="text-white h-96 w-80 ml-20 rounded-2xl flex justify-center border border-gold-accent bg-blue-accent">
+                <div class="mt-7">
+                    <p class="text-center text-2xl font-bold">Setup</p>
+                    <p class="text-center mt-3">It's quick and easy...</p>
+                    <h1 class="mt-5  primary-blue">1. <a href="#">Add to Discord</a></h1>
+                    <h1 class=""><a class="primary-blue">2.</a> Type "v!help" for all commands </h1>
+                    <h1 class=""><a class="primary-blue">3.</a> Enjoy the bot! </h1>
+                    <img class="mt-5" src="~/static/imgs/Cogs.svg" alt="Cogs and Squares">
+                    <button class="bg-red-accent p-3 rounded-xl ml-12 mt-3">Guides Coming Soon...</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="w-full mt-24 h-32 bg-blue-accent text-center text-white">
+    <div class="flex justify-center pt-8">
+        <a href="#"><img class="mr-2" src="~/static/imgs/twitter.svg" alt="Twitter Logo"></a>
+        <a href="#"><img class="mr-2" src="~/static/imgs/instagram.svg" alt="Instagram Logo"></a>
+        <a href="#"><img class="mr-2" src="~/static/imgs/github.svg" alt="Instagram Logo"></a>
+        <a href="#"><img class="mr-2" src="~/static/imgs/discord.svg" alt="Instagram Logo"></a>
+    </div>
+        <p class="pt-1">Copyright © 2021 Exult Games</p>
+        <p>All Rights Reserved</p>
     </div>
 </div>
 </template>
