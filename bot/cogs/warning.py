f40c2de21c24cdda517e638f5a77d47d411b1229
Andeh <andrewg.mason2709@gmail.com>
2022-02-20 23:26:30 +0000

    Converted database class
    Moved from warning database class to cases database class

--- bot/cogs/warning.py
+++ bot/cogs/warning.py
@@ -2,15 +2,17 @@ import discord
 from discord.ext import commands
 import kimetsu
 
+from datetime import datetime
 from typing import Union
 
+from database.cases import CasesDB
 from database.warnings import WarningDB
 from tools.components import Paginator
 
 class Warning(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.db = WarningDB(bot.db)
+        self.db = CasesDB(bot.db)
 
     def get_chunks(self, interval: int, array: list):
         """split up an array into a two dimensional array with the interval specified"""
@@ -30,10 +32,10 @@ class Warning(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def warn(self, ctx, member: discord.Member, reason: str):
+    async def warn(self, ctx: commands.Context, member: discord.Member, reason: str):
         """Warn a member"""
-        await self.db.add_warn(ctx.guild.id, member.id, ctx.author.id, reason)
-        embed = discord.Embed(description=f"**Reason:** {reason}\n**Warned at:** {kimetsu.parsedate.Parsedate().parsedate()}", colour=self.bot.red)
+        await self.db.add_case("warn", ctx.guild.id, member.id, ctx.author.id, reason, None)
+        embed = discord.Embed(description=f"**Reason:** {reason}\n**Warned at:** {kimetsu.Time.parsedate(ctx.message.created_at)}", colour=self.bot.red)
         embed.set_author(icon_url=member.avatar.url, name=f"{member.name} has been warned")
         embed.set_footer(icon_url=ctx.author.avatar.url, text=f"Warned by {ctx.author.name}")
         await ctx.send(embed=embed)
@@ -43,7 +45,7 @@ class Warning(commands.Cog):
     async def warns(self, ctx, member: discord.Member=None):
         """Displays all warnings for a given member"""
         member = ctx.author if member is None else member
-        member_warns = await self.db.get_warns_for_member(ctx.guild.id, member.id)
+        member_warns = await self.db.get_cases_for_member(ctx.guild.id, member.id, "warn")
         embed = discord.Embed(colour=self.bot.red).set_author(icon_url=member.avatar.url, name=f"{member.name}'s Warnings")
         if member_warns is None or member_warns == []:
             embed.description = f"{member} has no warnings!"
@@ -56,9 +58,11 @@ class Warning(commands.Cog):
                 embed = discord.Embed(colour=self.bot.red).set_author(icon_url=member.avatar.url, name=f"{member.name}'s Warnings")
                 for warn in warnpage:
                     warn_num +=1
+                    last_updated = "" if not warn[7] else f"\nLast updated: {kimetsu.Time.parsedate((datetime.fromisoformat(warn[7])))}"
                     value = f"Warning ID: {warn[0]}\n" \
                             f"Warned by: {self.bot.get_user(warn[3]).mention}\n" \
-                            f"Reason: {warn[4]}"  
+                            f"Reason: {warn[5]}\n" \
+                            f"Warned at: {kimetsu.Time.parsedate((datetime.fromisoformat(warn[6])))}{last_updated}"
                     embed.add_field(name=f"Warning #{warn_num}", value=value, inline=False)
                 embeds.append(embed)
         if len(embeds) > 1:
@@ -69,15 +73,15 @@ class Warning(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def delwarn(self, ctx, warning_id):
+    async def delwarn(self, ctx, warning_id: int):
         """Delete a warn via its ID"""
-        x = await self.db.get_warn(int(warning_id))
+        x = await self.db.get_case(warning_id)
         if not x:
             return await ctx.send(embed=discord.Embed(description=f"Could not find a warning with an ID of `{warning_id}`", colour=self.bot.red).set_author(name="Warning not Found"))
-        await self.db.remove(int(warning_id))
-        user = self.bot.get_user(x[2])
-        moderator = self.bot.get_user(x[3])
-        reason = x[4]
+        await self.db.remove_case(int(warning_id))
+        user = self.bot.get_user(x[3])
+        moderator = self.bot.get_user(x[4])
+        reason = x[6]
         description = f"**User:** {user.mention}\n**Moderator:** {moderator.mention}\n**Reason:** {reason}"
         await ctx.send(embed=discord.Embed(description=description, colour=self.bot.red).set_author(icon_url=user.avatar.url, name=f"Successfully deleted warning"))
 
@@ -85,13 +89,13 @@ class Warning(commands.Cog):
     @commands.has_permissions(manage_messages=True)
     async def delwarns(self, ctx, member: Union[discord.Member, str]):
         if isinstance(member, discord.Member):
-            x = await self.db.get_warns_for_member(ctx.guild.id, member.id)
+            x = await self.db.get_cases_for_member(ctx.guild.id, member.id, "warn")
             if type(x) == list:
                 total_warns = len(x)
-                await self.db.clear_member(ctx.guild.id, member.id)
+                await self.db.clear_member(ctx.guild.id, member.id, "warn")
         elif isinstance(member, str):
             if member.lower() == "all":
-                x = await self.db.clear_guild(ctx.guild.id)
+                x = await self.db.clear_guild(ctx.guild.id, "warn")
             else:
                 return
         if type(x) == int:
@@ -107,20 +111,18 @@ class Warning(commands.Cog):
 
     @commands.command()
     async def editwarn(self, ctx, warning_id: int, *, new_reason: str):
-        x = await self.db.get_warn(warning_id)
+        x = await self.db.get_case(warning_id)
         if x is None:
             return await ctx.send(embed=discord.Embed(description=f"Warning `{warning_id}` does not exist!", colour=self.bot.red).set_author(name="Warning not found"))
-        member = self.bot.get_user(x[2])
-        moderator = self.bot.get_user(x[3])
+        member = self.bot.get_user(x[3])
+        moderator = self.bot.get_user(x[4])
         old_reason = x[4]
-        await self.db.update(warning_id, new_reason)
+        await self.db.update_case(warning_id, new_reason)
         description = f"**Member:** {member.mention}\n" \
                       f"**Moderator:** {moderator.mention}\n" \
                       f"**Old Reason:**```\n{old_reason}\n```" \
                       f"**New Reason:**```\n{new_reason}\n```"
         await ctx.send(embed=discord.Embed(description=description, colour=self.bot.red).set_author(icon_url=member.avatar.url, name="Warning updated"))
-
-
 
 def setup(bot):
     bot.add_cog(Warning(bot))
