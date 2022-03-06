c47edfc2f5ead92cc1381174bc43f76fe9c516db
Andeh <andrewg.mason2709@gmail.com>
2022-03-06 02:28:20 +0000

    Purge main from rewrite

--- bot/config.py
+++
@@ -1,6 +0,0 @@
-TOKEN = ""
-PSQL_URI = "postgresql://" #Insert ur own psql uri here, tables and columns can be found in the readme.
-DAGPI_TOKEN = ""
-SPOTIFYSECRET = ""
-SPOTIFYCLIENT = ""
-lyric_api = ""
