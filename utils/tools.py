38f66009ce17c393130dcffeeb50d5437cc1b5df
Andeh <andrewg.mason2709@gmail.com>
2022-03-12 11:47:18 +0000

    Tools and Views Creation

---
+++ utils/tools.py
@@ -0,0 +1,102 @@
+import discord
+from typing import Union
+import re
+import datetime
+
+class hextype:
+    def __init__(self, x):
+        if isinstance(x, str):
+            self.val = int(x, 16)
+        elif isinstance(x, int):
+            self.val = int(str(x), 16)
+    def __str__(self):
+        return hex(self.val)
+
+def embed_builder(*, title:Union[str, None]=None, description:Union[str, None]=None, colour:hextype=0xfb5f5f, 
+                  timestamp:Union[bool, None]=None, author:Union[list, str, None]=None, footer:Union[list, str, None]=None,
+                  thumbnail:Union[str, None]=None, image:Union[str, None]=None, fields:Union[list, None]=None, url:str=None):
+    embed = discord.Embed()
+    if title: embed.title = title
+    if description: embed.description = description
+    if timestamp: embed.timestamp = timestamp
+    embed.colour = colour
+    if thumbnail: embed.set_thumbnail(url=thumbnail)
+    if image: embed.set_image(url=image)
+    if url: embed.url = url
+    if author:
+        if isinstance(author, list):
+            embed.set_author(icon_url=author[0], name=author[1])
+        elif isinstance(author, str):
+            embed.set_author(name=author)
+    if footer:
+        if isinstance(footer, list):
+            embed.set_footer(icon_url=footer[0], name=footer[1])
+        elif isinstance(footer, str):
+            embed.set_footer(text=footer)
+    if fields:
+        for field in fields:
+            try:
+                embed.add_field(name=field[0], value=field[1], inline=field[2])
+            except IndexError:
+                embed.add_field(name=field[0], value=field[1])
+    return embed
+
+class Time:
+    def handler(time):
+        secsearch = re.search("s?ec|s", time)
+        minsearch = re.search("m?in|m", time)
+        hoursearch = re.search("h?our|h", time)
+        daysearch = re.search("d?ay|d", time)
+        nums = re.findall("\d", time)
+        num = "".join(nums)
+
+        if secsearch:
+            dtype = "seconds"
+        if minsearch:
+            dtype = "minutes"
+        if hoursearch:
+            dtype = "hours"
+        if daysearch:
+            dtype = "days"
+        if int(num) == 1:
+            dtype = dtype[:-1]
+        return [num, dtype]
+
+    def parsedate(date=discord.utils.utcnow()):
+        timestamp = datetime.datetime.timestamp(date)
+        timestamp = str(timestamp).split('.', 1)[0]
+        return f"<t:{timestamp}:R>"
+
+    def formatdate(date=discord.utils.utcnow()):
+        return date.strftime("%d/%m/%Y %H:%M")
+
+    def convert_for_command(time: Union[datetime.datetime, list]):
+        if isinstance(time, list):
+            num = int(time[0])
+            ttype = str(time[1])
+            if ttype.startswith("s"):  
+                return datetime.datetime.utcnow() + datetime.timedelta(seconds=num)
+            elif ttype.startswith("mi"):
+                return datetime.datetime.utcnow() + datetime.timedelta(minutes=num)
+            elif ttype.startswith("h"):
+                return datetime.datetime.utcnow() + datetime.timedelta(hours=num)
+            elif ttype.startswith("d"):
+                return datetime.datetime.utcnow() + datetime.timedelta(days=num)
+        elif isinstance(time, datetime.datetime):
+            return datetime.datetime.utcnow() + time
+        
+def get_chunks(interval: int, array: list):
+    """split up an array into a two dimensional array with the interval specified"""
+    total = []
+    a = 0
+    if len(array) <= interval:
+        return [array]
+    for i in range(len(array) // interval):
+        chunks = []
+        for _ in range(interval):
+            chunks.append(array[a])
+            a += 1
+        total.append(chunks)
+    if len(array) % interval != 0:
+        total.append(array[a:])
+    return total
---
+++ utils/views.py
@@ -0,0 +1,70 @@
+from discord import ButtonStyle, Interaction
+from discord.ui import View, Button
+
+from utils.tools import embed_builder
+
+class Paginator(View):
+    def __init__(self, pages, page=0, start_end=False, step_10=False):
+        super().__init__(timeout=120)
+        self.page = page
+        self.pages = pages
+        self.count = len(pages)
+        self.start_end = start_end
+        self.step_10 = step_10
+        self.add_buttons()
+
+    def add_buttons(self):
+        non_page_buttons = [item for item in self.children if not isinstance(item, PaginatorButton)]
+        if self.children:
+            self.clear_items()
+        if not self.count or self.count == 1:
+            return
+        previous_page = self.page - 1
+        if previous_page < 0:
+            previous_page = self.count - 1
+        self.add_item(PaginatorButton(label="◀", page=previous_page, style=ButtonStyle.red))
+        self.add_item(PaginatorButton(label=f"{self.page + 1} / {len(self.pages)}", style=ButtonStyle.grey, disabled=True))
+        next_page = self.page + 1
+        if next_page > self.count - 1:
+            next_page = 0
+        self.add_item(PaginatorButton(label="▶", page=next_page, style=ButtonStyle.green))
+        for item in non_page_buttons:
+            self.add_item(item)
+            
+class PaginatorButton(Button["Paginator"]):
+    def __init__(self, label, style, row=0, page=None, disabled=False):
+        super().__init__(style=style, label=label, row=row, disabled=disabled)
+        self.page = page
+
+    async def callback(self, interaction: Interaction):
+        self.pages = self.view.pages
+        self.view.page = self.page
+        self.view.add_buttons()
+        await interaction.message.edit(embed=self.pages[self.page], view=self.view)
+        
+class AvatarButton(Button):
+    def __init__(self, label, style, custom_id):
+        super().__init__(style=style, label=label, custom_id=custom_id)
+        
+    async def callback(self, interaction: Interaction):
+        custom_id = interaction.data["custom_id"]
+        member = await interaction.client.fetch_user(int(custom_id[6:]))
+        await interaction.message.edit(embed=embed_builder(author=[member.display_avatar.url, f"{member.name}'s Avatar"],
+                                                            image=member.display_avatar.url))
+
+class BannerButton(Button):
+    def __init__(self, label, style, custom_id):
+        super().__init__(style=style, label=label, custom_id=custom_id)
+        
+    async def callback(self, interaction: Interaction):
+        custom_id = interaction.data["custom_id"]
+        member = await interaction.client.fetch_user(int(custom_id[6:]))
+        await interaction.message.edit(embed=embed_builder(author=[member.display_avatar.url, f"{member.name}'s Banner"],
+                                                            image=member.banner.url))
+        
+class Avatar(View):
+    def __init__(self, member):
+        member = member
+        super().__init__()
+        self.add_item(AvatarButton("View Avatar", ButtonStyle.secondary, f"avatar{member.id}"))
+        self.add_item(BannerButton("View Banner", ButtonStyle.secondary, f"banner{member.id}"))
